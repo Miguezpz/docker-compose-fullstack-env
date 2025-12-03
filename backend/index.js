9c2542e8f478e43a913193617df6c833cc2096c9
Miguezpz <miguezpzmx@gmail.com>
2025-12-03 08:45:14 -0600

    Files were added.

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const myPort = 3000;
+const generatedNumbers = [];
+
+app.use(express.json());
+app.use(cors());
+
+// Ruta para generar un número aleatorio
+app.get('/my-app/generateNumber', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    generatedNumbers.push(randomNumber);
+
+    console.log(`Random number was generated -> ${randomNumber}`);
+    res.send({ generated: randomNumber });
+});
+
+// Ruta para ver todos los números generados
+app.get('/my-app/seeNumbers', (req, res) => {
+    res.send(generatedNumbers);
+});
+
+app.listen(myPort, '0.0.0.0', () => {
+    console.log(`running server on http://localhost:${myPort}`);
+});
+
